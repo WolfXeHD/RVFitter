5aa88f8eeef649f2a4db87a92bddda292d298b81
Tim Wolf <tim.wolf@mpi-hd.mpg.de>
2022-11-22 21:00:26 +0100

    Adds first utils

---
+++ RVFitter/utils.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+def read_line_list(filename):
+    with open(filename, "r") as f:
+        data = f.read()
+
+    lines = []
+    for line in data.splitlines():
+        if line.startswith("#"):
+            continue
+        splitted_line = line.split(" ")
+        this_dict = {}
+        this_dict["line_name"] = splitted_line[0]
+        this_dict["line_profile"] = float(splitted_line[1])
+        this_dict["line_width"] = float(splitted_line[2])
+        lines.append(this_dict)
+    return lines
+
+def manipulate_df_by_line_list(df, line_list):
+    if line_list is not None:
+        l_dfs = []
+        for line in line_list:
+            this_df = df.query(
+                f"(line_name == \"{line['line_name']}\") & (line_profile == {line['line_profile']})"
+            )
+            if len(this_df) == 0:
+                df_name = df.query(
+                    f"(line_name == \"{line['line_name']}\")"
+                )
+
+                if len(df_name) == 0:
+                    available_lines = df["line_name"].unique()
+                    raise Exception(
+                        f"Line {line['line_name']} not found in dataframe. Available lines are: {available_lines}")
+                else:
+                    available_line_profiles = df_name["line_profile"].unique()
+                    raise Exception("No data for line {} with profile {}. Available profiles are {}".format(
+                        line["line_name"], line["line_profile"], available_line_profiles))
+
+            l_dfs.append(this_df)
+        df = pd.concat(l_dfs)
+    else:
+        print("No line list provided. Using all lines in dataframe.")
+    return df
