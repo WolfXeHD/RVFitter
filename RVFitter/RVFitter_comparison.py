b524bce42b18b92a026cc762bc39f09c01ffb7b8
M.C. Ramirez-Tannus <maclarata@gmail.com>
2022-11-25 10:47:32 +0100

    adds option to show plot before saving and to change figure names

--- RVFitter/RVFitter_comparison.py
+++ RVFitter/RVFitter_comparison.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import sigfig
+import ipdb
 
 
 class RVFitter_comparison(object):
@@ -187,7 +188,7 @@ class RVFitter_comparison(object):
             val = sigfig.round(x[value_var], x[error_var], separation=' \pm ')
         return "$" + val + "$"
 
-    def compare_fit_results_1D(self, variable, fig_and_ax=None):
+    def compare_fit_results_1D(self, variable, fig_and_ax=None, suffix=None):
         if variable not in ["amp", "cen", "sig"]:
             raise Exception("Error: variable not in ['amp', 'cen', 'sig']")
 
@@ -213,6 +214,7 @@ class RVFitter_comparison(object):
             #sort dataframe by column
             this_df["line_profile"] = this_df["line_profile"].astype(int)
             this_df = this_df.sort_values(by='line_profile')
+            
             labels = this_df["line_name"].values
             this_df["labels"] = this_df.apply(
                 lambda x: x["line_name"] + " " + str(x["line_profile"]), axis=1)
@@ -236,16 +238,21 @@ class RVFitter_comparison(object):
         handles, labels = axes[-1].get_legend_handles_labels()
         if variable == 'cen':
             axes[-1].set_xlabel('Velocity (km/s)')
-            filename = "compare_results_velocity.png"
+            if suffix == None:
+                filename = "compare_results_velocity.png"
+            else:
+                print(suffix)
+                filename = suffix + "_compare_results_velocity.png"
         else:
             axes[-1].set_xlabel(variable)
             filename = "compare_results_{variable}.png"
 
         fig.legend(handles, labels, ncol=2, loc='lower center')
-
         this_filename = os.path.join(self.output_folder, filename)
+
         fig.savefig(this_filename)
         print(this_filename, "saved.")
+        plt.show()
         plt.close(fig)
 
     def plot_fits_and_residuals(self,
